6ea061d932a1a7c6880ee36b7a909106d0049c07
Tomaz <luizfelipe123924@gmail.com>
2025-04-24 23:57:29 -0300

    Criando o T

---
+++ t.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define altura 100
+#define largura 100
+
+unsigned char img[altura][largura][3];
+
+void inicializarimg(){
+    
+    printf("P3\n%d\t%d\n255\n", altura, largura);
+
+    for(int y = 0; y < altura; y++){
+        for(int x = 0; x < largura; x++){
+               img[y][x][0] = 255;
+               img[y][x][1] = 255;
+               img[y][x][2] = 255;
+            }
+        }
+    }
+
+
+
+void alterarpixel (int y, int x, unsigned char r, unsigned char g, unsigned char b){
+    img[y][x][0] = r;
+    img[y][x][1] = g;
+    img[y][x][2] = b;
+}
+
+void imprimirimg (){
+    for(int y = 0; y < altura; y++){
+        for(int x = 0; x < largura; x++){
+            printf("%d\t%d\t%d\n", img[y][x][0], img[y][x][1], img[y][x][2] );
+        }
+    }
+}
+
+void t(){
+    for(int y = 0; y < altura; y++){
+            for(int x = 30; x < largura - 2; x++){
+                if( y == 29){
+                    alterarpixel(x, y, 247, 100, 79);
+                if( x == 49){
+                    alterarpixel(x, y, 247, 100, 79);
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    inicializarimg(img);
+    t(img);
+    imprimirimg(img);
+    return 0;
+}
